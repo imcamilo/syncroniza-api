e8cc374a3f190a41f52f45edcb9ace978636e33e
Camilo <camilo.jorquera.a@gmail.com>
2019-04-01 16:10:23 -0300

    feat(add controller and service)

---
+++ src/main/groovy/syncroniza/cl/object/controller/PlayerController.groovy
@@ -0,0 +1,23 @@
+package syncroniza.cl.object.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import syncroniza.cl.object.dto.PlayerDTO
+import syncroniza.cl.object.service.PlayerService
+
+@RestController
+@RequestMapping("/api/v1/players")
+class PlayerController {
+
+    @Autowired
+    PlayerService playerService
+
+    @PostMapping
+    def create(@RequestBody PlayerDTO playerDTO) {
+        playerService.create(playerService)
+    }
+
+}
--- src/main/groovy/syncroniza/cl/object/service/PlayerServiceImp.groovy
+++ src/main/groovy/syncroniza/cl/object/service/PlayerServiceImp.groovy
@@ -1,11 +1,13 @@
 package syncroniza.cl.object.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 import syncroniza.cl.object.Player
 import syncroniza.cl.object.dto.PlayerDTO
 import syncroniza.cl.object.mapper.PlayerMapper
 import syncroniza.cl.object.util.DTOMapper
 
+@Service
 class PlayerServiceImp implements PlayerService {
 
     @Autowired
