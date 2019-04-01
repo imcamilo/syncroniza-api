e0480f53b97b7ce715e4ef15fb86a60e761b3dcb
Camilo <camilo.jorquera.a@gmail.com>
2019-04-01 12:46:01 -0300

    Initial commit

---
+++ src/main/groovy/syncroniza/cl/object/SyncronizaApplication.groovy
@@ -0,0 +1,13 @@
+package syncroniza.cl.object
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SyncronizaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SyncronizaApplication, args)
+	}
+
+}
